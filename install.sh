5294a1527015390f323b45c2b11d02d471423e1c
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-09-08 23:05:59 +0200

    (s)tuff

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 sudo systemctl restart nix-daemon.service
 echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
 
+# Apply Flake
+nix run home-manager/master -- switch --flake . -b backup
---
+++ update.sh
@@ -0,0 +1,2 @@
+git pull
+home-manager switch --flake .
