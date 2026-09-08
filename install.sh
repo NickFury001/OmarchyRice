20ad572228aa90108629c21c0e5a68901082b1e2
NickFury001 <95432249+NickFury001@users.noreply.github.com>
2026-09-08 23:54:03 +0200

    (s)tuff

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Install Nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
+source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 sudo systemctl restart nix-daemon.service
 echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
 
